66f48e2fdaa3f16bc52c40360ca79b8df5fce68e
Jason <jason@scurker.com>
2021-03-09 09:33:46 -0600

    formatting

--- src/currency.d.ts
+++ src/currency.d.ts
@@ -1,42 +1,42 @@
 declare module 'currency.js' {
-    namespace currency {
-        type Any = number | string | currency;
-        type Format = (currency?: currency, opts?: Options) => string;
-        interface Constructor {
-            (value: currency.Any, opts?: currency.Options): currency,
-            new(value: currency.Any, opts?: currency.Options): currency
-        }
-        interface Options {
-            symbol?: string,
-            separator?: string,
-            decimal?: string,
-            errorOnInvalid?: boolean,
-            precision?: number,
-            increment?: number,
-            useVedic?: boolean,
-            pattern?: string,
-            negativePattern?: string,
-            format?: currency.Format,
-            fromCents?: boolean
-        }
+  namespace currency {
+    type Any = number | string | currency;
+    type Format = (currency?: currency, opts?: Options) => string;
+    interface Constructor {
+      (value: currency.Any, opts?: currency.Options): currency,
+      new(value: currency.Any, opts?: currency.Options): currency
     }
-
-    interface currency {
-        add(number: currency.Any): currency;
-        subtract(number: currency.Any): currency;
-        multiply(number: currency.Any): currency;
-        divide(number: currency.Any): currency;
-        distribute(count: number): Array<currency>;
-        dollars(): number;
-        cents(): number;
-        format(opts?: currency.Options | currency.Format): string;
-        toString(): string;
-        toJSON(): number;
-        readonly intValue: number;
-        readonly value: number;
+    interface Options {
+      symbol?: string,
+      separator?: string,
+      decimal?: string,
+      errorOnInvalid?: boolean,
+      precision?: number,
+      increment?: number,
+      useVedic?: boolean,
+      pattern?: string,
+      negativePattern?: string,
+      format?: currency.Format,
+      fromCents?: boolean
     }
+  }
 
-    const currency: currency.Constructor;
+  interface currency {
+    add(number: currency.Any): currency;
+    subtract(number: currency.Any): currency;
+    multiply(number: currency.Any): currency;
+    divide(number: currency.Any): currency;
+    distribute(count: number): Array<currency>;
+    dollars(): number;
+    cents(): number;
+    format(opts?: currency.Options | currency.Format): string;
+    toString(): string;
+    toJSON(): number;
+    readonly intValue: number;
+    readonly value: number;
+  }
 
-    export = currency;
+  const currency: currency.Constructor;
+
+  export = currency;
 }
